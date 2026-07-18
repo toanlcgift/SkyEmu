dcf8c6923aa52ba71799a343e23bfd81c68ac21c
toanlcgift <toanlcgift@gmail.com>
2026-07-18 17:48:15 +0700

    fix degrade android

--- src/sokol/sokol_app.h
+++ src/sokol/sokol_app.h
@@ -7550,13 +7550,6 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 
 /*== Android ================================================================*/
 #if defined(_SAPP_ANDROID)
-sapp_desc sokol_main(int argc, char* argv[]) {
-    _SOKOL_UNUSED(argc);
-    _SOKOL_UNUSED(argv);
-    sapp_desc desc;
-    memset(&desc, 0, sizeof(desc));
-    return desc;
-}
 /* android loop thread */
 _SOKOL_PRIVATE bool _sapp_android_init_egl(void) {
     SOKOL_ASSERT(_sapp.android.display == EGL_NO_DISPLAY);
