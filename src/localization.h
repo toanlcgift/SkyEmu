844eb43ba3a8983672d075323310a41557802670
toanlcgift <toanlcgift@gmail.com>
2026-08-22 14:28:22 +0700

    update

--- src/localization.h
+++ src/localization.h
@@ -23,9 +23,11 @@
 #define SE_LANG_SWEDISH 81
 #define SE_MAX_LANG_VALUE 85
 
-void se_set_language(int language_enum);//i.e. SE_LANG_ENGLISH
-const char* se_language_string(int language_enum);//returns "" if language is not supported
-const char* se_localize(const char* string);
-int se_convert_locale_to_enum(const char* clocale);
+#include "skyemu_dll.h"
+
+SKYEMU_API void se_set_language(int language_enum);//i.e. SE_LANG_ENGLISH
+SKYEMU_API const char* se_language_string(int language_enum);//returns "" if language is not supported
+SKYEMU_API const char* se_localize(const char* string);
+SKYEMU_API int se_convert_locale_to_enum(const char* clocale);
 
 #endif
